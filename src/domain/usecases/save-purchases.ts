f83703a78f7086ec3d4dcecf969258321a987af6
Marcos Tomaz <marcosvto1@gmail.co>
2020-06-26 22:35:18 -0300

    add SavePurchases use case

---
+++ src/domain/usecases/save-purchases.ts
@@ -0,0 +1,14 @@
+export interface SavePurchases {
+    save: (purchades: Array<SavePuerchases.Params>) => Promise<void>;
+
+
+}
+
+namespace SavePuerchases {
+
+    export type Params = {
+        id: string,
+        date: Date
+        value: number
+    }
+}
