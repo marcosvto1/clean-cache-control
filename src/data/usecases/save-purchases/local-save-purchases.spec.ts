6252fc8b94de2c597c8654edf20f89fc7f18a210
Marcos Tomaz <marcosvto1@gmail.co>
2020-06-28 17:11:56 -0300

    ensure LocalSavePurchases will not insert new Cache if delete fails

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -4,6 +4,7 @@ import {LocalSavePurchases} from '@/data/usecases';
 
 class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0;
+    insertCallsCount = 0;
     key: string;
 
     delete(key: string): void {
@@ -28,11 +29,19 @@ describe('LocalSavePurchases', () => {
         expect(cacheStore.deleteCallsCount).toBe(0)
     })
 
-    test('Should delete old cache on sut.save', () => {
+    test('Should delete old cache on sut.save', async () => {
         const {cacheStore, sut} = makeSut();
-        sut.save();
+        await sut.save();
         expect(cacheStore.deleteCallsCount).toBe(1)
         expect(cacheStore.key).toBe('purchases')
+    })
+
+    test('Should not Insert new Cache if delete fails', async () => {
+        const {cacheStore, sut} = makeSut();
+        jest.spyOn(cacheStore, 'delete').mockImplementationOnce(() => {throw new Error()});
+        const promise = sut.save();
+        expect(cacheStore.insertCallsCount).toBe(0)
+        expect(promise).rejects.toThrow();
     })
 
 })
--- src/domain/usecases/save-purchases.ts
+++ src/domain/usecases/save-purchases.ts
@@ -1,11 +1,8 @@
 export interface SavePurchases {
     save: (purchades: Array<SavePuerchases.Params>) => Promise<void>;
-
-
 }
 
 namespace SavePuerchases {
-
     export type Params = {
         id: string,
         date: Date
